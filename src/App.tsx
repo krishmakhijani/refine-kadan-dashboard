ceb3cfb3dd6241c8c8294d5b230e9b45918e027b
Krish Makhijani <112251957+velocitypanther@users.noreply.github.com>
2024-02-29 01:33:21 +0530

    Added company create company component functionality

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
 
 function App() {
   return (
@@ -56,7 +57,10 @@ function App() {
                     </Layout>
                     </Authenticated>}>
                     <Route index element={<Home/>} />
-                    <Route path="/companies" element={<CompanyList/>} />
+                    <Route path="/companies" >
+                      <Route index element={<CompanyList/>} />
+                      <Route path="new" element={<Create/>}/>
+                    </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
